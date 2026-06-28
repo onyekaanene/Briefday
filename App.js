96fdb5631f807561bde7068ceece4bef71a00e12
Onyeka <onyekanene@gmail.com>
2026-06-28 12:12:32 +0100

    fix: add comment to clarify the main entry point of the application in App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AppNavigator from "./navigation/AppNavigator";
 
+// This is the main entry point of the application. It imports the AppNavigator component from the navigation folder and renders it as the root component of the app.
 export default function App() {
   return <AppNavigator />;
 }
